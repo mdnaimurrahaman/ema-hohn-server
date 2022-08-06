9b568324d2511d043b5d22b6afbf49fee184bf6c
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-08-06 21:13:46 +0600

    new api added

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('John is running an waiting for ema')
 });
+app.post('/', (req,res)=>{
+    res.send('John is running an waiting for ema')
+});
 
 app.listen(port,()=>{
     console.log('John is running on port',port);
